1dbde6048078b33878416431b14539c4a41455bd
Aryan9021 <aryanbarbate3@gmail.com>
2026-06-06 21:13:19 +0530

    Update variable.js

--- Day2/variable.js
+++
@@ -1,8 +0,0 @@
-
-//let == redecleration = impossible , reinitialization = possible . hoisting=impossible
-//const== redecleration = impossible , reinitialization = impossible . hoisting=impossible
-let a = "hello world";
-let a = 20;
-
-
-console.log(a);
